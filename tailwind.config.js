8a48da57ce7ee2a9e1cd41316c71fe99ec17b785
Mikael <luanmikaelmartins@gmail.com>
2025-04-03 01:00:11 -0300

    chore: add new colors and gradients in tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        gray: {
+          dark: "161513",
+          softdark: "#191919",
+          light: "222222",
+          softlight: "2A2A2A",
+          lighter: "8491A0",
+        },
+      },
+      backgroundImage: {
+        "primary-gradient": "linear-gradient(to right, #FF8660, #9A33FF)",
+        "orange-gradient": "linear-gradient(to right, #FF8660, #D5491D)",
+        "blue-gradient": "linear-gradient(to right, #5BADFF, #1373D1)",
+      },
+    },
   },
   plugins: [],
-}
+};
