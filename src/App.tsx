643e841ab8195c444d1750419383cef22ef0333b
Mikael <luanmikaelmartins@gmail.com>
2025-04-13 17:11:49 -0300

    feat: complete app interface

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,30 @@
 import { Navbar } from './sections/navbar'
 import { About } from './sections/home'
+import { Projects } from './sections/projects'
+import { Experience } from './sections/experience'
+import { Footer } from './sections/footer'
 
 function App() {
   return (
     <>
       <Navbar />
-      <main className="flex flex-col items-center px-60 py-48 gap-20">
-        <About />
+      <main className="flex flex-col items-center px-60">
+        <section
+          className="flex min-h-screen w-full items-center justify-center"
+          id="home"
+        >
+          <About />
+        </section>
+        <section className="w-full scroll-mt-24" id="projects">
+          <Projects />
+        </section>
+        <section className="w-full" id="experience">
+          <Experience />
+        </section>
       </main>
+      <section className="w-full" id="contact">
+        <Footer />
+      </section>
     </>
   )
 }
